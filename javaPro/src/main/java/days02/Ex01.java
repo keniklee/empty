4b645c473f3ec254409b2fc56914f88e3cb3eb87
SIST163 <SIST163@SIST163>
2020-02-18 14:57:37 +0900

    2 commit.

---
+++ javaPro/src/main/java/days02/Ex01.java
@@ -0,0 +1,14 @@
+package days02;
+
+/**
+ * @author SIST163
+ *
+ */
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("> days02.Ex01.java");
+	}
+
+}
