26b7e79a08e204ca95c5e4149889e927c5f498e9
SIST163 <SIST163@SIST163>
2020-02-18 15:01:24 +0900

    board branch - list/write

---
+++ javaPro/src/main/java/board/List.java
@@ -0,0 +1,11 @@
+package board;
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("> Board Write <");
+
+	}
+
+}
---
+++ javaPro/src/main/java/board/Write.java
@@ -0,0 +1,11 @@
+package board;
+
+public class Write {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("> Board List <");
+
+	}
+
+}
