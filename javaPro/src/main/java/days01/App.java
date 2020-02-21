5dbc0a759b55322bba9f4d259211f7a200d7c373
SIST163 <SIST163@SIST163>
2020-02-21 10:41:27 +0900

    커밋 테스트 용도 코딩 추가....수정

--- javaPro/src/main/java/days01/App.java
+++ javaPro/src/main/java/days01/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        //  Git 설명을 위해 한 줄 코딩 수정.....
+        System.out.println( "Hello World!" );        
     }
 }
