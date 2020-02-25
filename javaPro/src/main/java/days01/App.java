4f355ae15344985c14f9cc448c4b53b2b42d2ba0
keniklee <kenik@naver.com>
2020-02-25 15:19:14 +0900

    ignored Resources

--- javaPro/src/main/java/days01/App.java
+++ javaPro/src/main/java/days01/App.java
@@ -15,5 +15,13 @@ public class App
         
         // Synchronize WorkSpace 테스트 -> OverWrite 
         // 서버 수정하고 동기화 워크스페이스 하면 .. 수정  된 부분 확인 후 덮어쓰기 기능
+        
+        // 이클립스에서 특정폴더 및 파일 버전 관리 대상에서 제외하는 방법
+        // .classpath 제외
+        //  1. Window>Perferences
+        //  2. Team>Git>Ignored Resources
+        //          Add Pattern
+        //             */classes
+        //             */classes/**
     }
 }
