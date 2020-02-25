4af49f04798c9703498e6e249bd29b7a3eb317a1
keniklee <kenik@naver.com>
2020-02-25 15:12:18 +0900

    Synchronize WorkSpace 테스트 -> OverWrite

--- javaPro/src/main/java/days01/App.java
+++ javaPro/src/main/java/days01/App.java
@@ -11,6 +11,9 @@ public class App
         System.out.println( "Hello World!" );
         
         //  Git 설명을 위해 한 줄 코딩 수정.....
-        System.out.println( "Hello World!" );        
+        System.out.println( "Hello World!" );       
+        
+        // Synchronize WorkSpace 테스트 -> OverWrite 
+        // 서버 수정하고 동기화 워크스페이스 하면 .. 수정  된 부분 확인 후 덮어쓰기 기능
     }
 }
